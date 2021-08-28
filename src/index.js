31682276d392519849732f035539676d280bb062
Ivan Strizhko <62095188+dante89919001@users.noreply.github.com>
2021-08-28 15:13:02 +0600

    Update index.js
    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let Array = [];
+
+    for( let i = 0; i < str.length; i++ ){
+        for ( let bracket of bracketsConfig ) {
+            if( str[i] == bracket[0]  && bracket[0] == bracket[1]
+                && bracket[0] == Array[Array.length - 1] ){
+                Array.pop();
+                break;
+            }
+            if ( str[i] == bracket[0] ){
+                Array.push( str[i] );
+                break;
+            }
+            if( str[i] == bracket[1] ){
+                if( bracket[0] != Array.pop() ){
+                    return false;
+                }
+            }
+        }
+    }
+    if( Array.length ) return false;
+    return true;
 }
